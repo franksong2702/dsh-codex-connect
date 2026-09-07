eca1c074a0bde009241b4b20ae6265730ed477fc
Frank Song <xuefu.song@qq.com>
2026-09-07 16:01:22 +0800

    chore: prepare alpha 4.31 network recovery release (#159)
    * chore: prepare alpha 4.31 network recovery release
    
    * test: include alpha 4.31 in verified catalog expectation
    
    ---------
    
    Co-authored-by: Frank Song <franksong2702@gmail.com>

--- tests/update-compatibility.spec.ts
+++ tests/update-compatibility.spec.ts
@@ -27,6 +27,7 @@ const catalog = {
     { version: '0.1.0-alpha.4.28', verifiedDshVersions: ['0.1.2-rc.1'] },
     { version: '0.1.0-alpha.4.29', verifiedDshVersions: ['0.1.2-rc.1'] },
     { version: '0.1.0-alpha.4.30', verifiedDshVersions: ['0.1.2-rc.1'] },
+    { version: '0.1.0-alpha.4.31', verifiedDshVersions: ['0.1.2-rc.1'] },
   ],
 }
 
