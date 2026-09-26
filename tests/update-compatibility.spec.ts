fc7c448179b930e8952f5476845b1fbf09ac68a1
Frank Song <franksong2702@gmail.com>
2026-09-26 10:40:07 +0800

    release: prepare alpha 4.49 with independent image fix review

--- tests/update-compatibility.spec.ts
+++ tests/update-compatibility.spec.ts
@@ -42,6 +42,7 @@ const catalog = {
     { version: '0.1.0-alpha.4.46', verifiedDshVersions: ['0.1.7-rc.1'] },
     { version: '0.1.0-alpha.4.47', verifiedDshVersions: ['0.1.7-rc.1'] },
     { version: '0.1.0-alpha.4.48', verifiedDshVersions: ['0.1.7-rc.1'] },
+    { version: '0.1.0-alpha.4.49', verifiedDshVersions: ['0.1.7-rc.1'] },
   ],
 }
 
